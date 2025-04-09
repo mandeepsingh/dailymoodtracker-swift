11d775074c2db07ba95dd8a0352aa97d7243c2a7
Mandeep Singh <mandeep.wsu@gmail.com>
2025-04-09 00:02:45 -0400

    Initial Commit

---
+++ DailyMoodTracker/DailyMoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMoodTrackerApp.swift
+//  DailyMoodTracker
+//
+//  Created by Mandeep Singh on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyMoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyMoodTrackerTests/DailyMoodTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMoodTrackerTests.swift
+//  DailyMoodTrackerTests
+//
+//  Created by Mandeep Singh on 4/9/25.
+//
+
+import Testing
+@testable import DailyMoodTracker
+
+struct DailyMoodTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
